7a5bb4541cfb7944167e23a67f30bf541bd1f331
Amazingwhite <artemzinovev688@gmail.com>
2021-07-21 18:07:53 +0300

    не работает

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import React, { useState } from "react";
 import { useForm } from "react-hook-form";
 import UserInfo from "./components/UserInfo";
 import { ErrorMessage } from "@hookform/error-message";
-import { getMinutes, getHours, getDate, getMonth, getYear, yearsToMonths, setHours, isAfter, setMinutes, isEqual } from 'date-fns'
+import { getMinutes, getHours, getDate, getMonth, getYear, yearsToMonths, isAfter, isEqual, set, differenceInMonths, differenceInDays, differenceInYears, getDaysInYear, getDaysInMonth, monthsToYears, addYears, differenceInMinutes, } from 'date-fns'
+import ru from "date-fns/locale/ru"
 import './App.css'
 
 export default function App() {
@@ -16,104 +17,60 @@ export default function App() {
   const countYears = (d1, d2) => (yearsToMonths(getYear(d1)) + (getMonth(d1) + 1) - (yearsToMonths(getYear(d2)) + (getMonth(d2) + 1))) / 12
 
   const timeCounter = (date1, date2) => {
-    date1 = new Date(date1);
-    date2 = setHours(new Date(date2), getHours(new Date()))
-    date2 = setMinutes(new Date(date2), getMinutes(new Date()))
-    let time = [];
-    let years = !isAfter(date1, date2) ? Math.ceil(countYears(date1, date2)) : Math.floor(countYears(date1, date2))
-    let months = (getMonth(date1) + 1) - (getMonth(date2) + 1)
-    let days = getDate(date1) - getDate(date2)
-    let hours = getHours(date1) - getHours(new Date())
-    let minutes = getMinutes(date1) - getMinutes(new Date())
-    setBeforeAfter(date1 >= date2)
+    let testDate1 = new Date(date1)
+    let testDate2 = new Date(date2)
 
-    if (isAfter(date1, date2) || isEqual(date1, date2)) {
-      if (months < 0 ) months = 12 + months
-      if (days < 0) {
-        days = 32 - new Date(2021, getMonth(date1) - 1, 32).getDate() + days;
-        months =  months - 1 
-        if (months <= 0 && years !==0) {
-          years -= 1 
-          months =  11 
-        }
-      }
-      if( hours < 0) {
-        days = days - 1
-        hours = 24 + hours
-        if( days < 0) {
-          months -= 1
-          days = 32 - new Date(2021, getMonth(date1) - 1, 32).getDate() + days
-        }
-        if(months < 0) {
-          years -= 1
-          months = 12 + months
-        }
-      }
-      if (minutes < 0) {
-        hours = hours - 1
-        minutes = 60 + minutes
-        if( hours < 0) {
-          days -= 1
-          hours = 24 + hours
-          if( days < 0) {
-            months -= 1
-            days = 32 - new Date(2021, getMonth(date1) - 1, 32).getDate() + days
+    let diffMinutes = getMinutes(testDate2) - getMinutes(testDate1)
+    let diffHours = getHours(testDate2) - getHours(testDate1)
+    let diffDays = differenceInMinutes(testDate2, testDate1)/60/24 
+    console.log(diffDays)
+    let diffYears = differenceInYears(testDate2, testDate1)                       
+    let diffMonths = differenceInMonths(testDate2, testDate1) - diffYears*12      
+
+    const daysInMonthCounter = (date1, date2) => {
+      let date1Month = getMonth(date1)
+      let date2Month = getMonth(date2)
+      let date1Year = getYear(date1)
+      let date2Year = getYear(date2)
+      let fullMonthsList = 0
+
+      for(let i = date1Year; i < date2Year; i++) {
+        if(i === date1Year){
+          for(let j = date1Month+1; j<=11; j++) {
+            fullMonthsList += getDaysInMonth(new Date(i, j, 1))
           }
-          if(months < 0) {
-            years -= 1
-            months = 12 + months
+        } else if (i === date2Year) {
+          for(let j = 0; j<date2Month; j++) {
+            fullMonthsList += getDaysInMonth(new Date(i, j, 1))
+          }
+        } else {
+          for(let j = 0; j<=11; j++) {
+            fullMonthsList += getDaysInMonth(new Date(i, j, 1))
           }
         }
       }
-    } else if (!isAfter(date1, date2)) {
-      if (months > 0) months = months - 12
-      if (days > 0) {
-        days = days - (32 - new Date(2021, getMonth(date2) - 1, 32).getDate())
-        months += 1 
-        if (months >= 0 && years !==0) {
-          years += 1 
-          months = months - 12 
-        }
-      }
-      if( hours > 0) {
-        days += 1
-        hours = hours - 24
-        if( days > 0) {
-          months += 1
-          days = -(32 - new Date(2021, getMonth(date1) - 1, 32).getDate() - days)
-        }
-        if(months > 0) {
-          years += 1
-          months -= 12
-        }
-      }
-      if (minutes > 0) {
-        hours = hours + 1
-        minutes = minutes - 60
-        if( hours > 0) {
-          days += 1
-          hours -= 24
-          if( days > 0) {
-            months = months + 1
-            days = -(32 - new Date(2021, getMonth(date1) - 1, 32).getDate() - days)
-          }
-          if(months > 0) {
-            years += 1
-            months -= 12
-          }
-        }
-      }
+      console.log(fullMonthsList);
+      return fullMonthsList;
     }
-    time.push({ years: years })
-    time.push({ months: months })
-    time.push({ days: days })
-    time.push({ hours: hours })
-    time.push({ minutes: minutes })
-    setUntilEvent(time)
+    diffDays -= daysInMonthCounter(testDate1, testDate2)
+    if(diffHours < 0) {
+      diffDays--
+      diffHours +=24
+    }
+    if(diffMinutes < 0) {
+      diffHours--
+      diffMinutes += 60
+    }
+    let countedDiff = [diffYears, diffMonths, Math.floor(diffDays), diffHours, diffMinutes]
+    console.log(countedDiff)
+    setUntilEvent(countedDiff)
   }
+
   const onSubmit = (data) => {
     let userAge = diffDates(new Date(data.currentDate), new Date(data.birthDate))
-    timeCounter(data.valuableEvent, data.currentDate)
+    let currentDate = set(new Date(data.currentDate), {hours: getHours(new Date()), minutes:  getMinutes(new Date())})
+    let valuableEvent = new Date(data.valuableEvent)
+    timeCounter(currentDate, valuableEvent)
     if (userAge > 0 && userAge < 130) {
       setAge(userAge)
       setIsSubmited(true)
--- src/components/UserInfo.jsx
+++ src/components/UserInfo.jsx
@@ -6,8 +6,8 @@ let UserInfo = (props) => {
 
         <h1>Возраст пользователя: {props.age} years</h1>
         <h1>
-            {props.beforeAfter && `До события ${props.untilEvent[0].years} years, ${props.untilEvent[1].months} months, ${props.untilEvent[2].days} days, ${props.untilEvent[3].hours} hours: ${props.untilEvent[4].minutes} minutes`} 
-            {!props.beforeAfter && `После события прошло ${-props.untilEvent[0].years} years, ${-props.untilEvent[1].months} months, ${-props.untilEvent[2].days} days, ${-props.untilEvent[3].hours} hours: ${-props.untilEvent[4].minutes} minutes`} 
+            {props.beforeAfter && `До события ${props.untilEvent[0]} years, ${props.untilEvent[1]} months, ${props.untilEvent[2]} days, ${props.untilEvent[3]} hours: ${props.untilEvent[4]} minutes`} 
+            {!props.beforeAfter && `После события прошло ${-props.untilEvent[0]} years, ${-props.untilEvent[1]} months, ${-props.untilEvent[2]} days, ${-props.untilEvent[3]} hours: ${-props.untilEvent[4]} minutes`} 
         </h1>
     </>
     )

cedd2681cb7b215ed24f269cdfced79155b898c0
Artem Zinovev <artemzinovev688@gmail.com>
2021-07-22 14:16:46 +0300

    date v2

--- src/App.js
+++ src/App.js
@@ -2,8 +2,7 @@ import React, { useState } from "react";
 import { useForm } from "react-hook-form";
 import UserInfo from "./components/UserInfo";
 import { ErrorMessage } from "@hookform/error-message";
-import { getMinutes, getHours, getDate, getMonth, getYear, yearsToMonths, isAfter, isEqual, set, differenceInMonths, differenceInDays, differenceInYears, getDaysInYear, getDaysInMonth, monthsToYears, addYears, differenceInMinutes, differenceInHours, minutesToHours, setMonth, } from 'date-fns'
-import ru from "date-fns/locale/ru"
+import { getMinutes, getHours, getMonth, getYear, isAfter, set, getDaysInMonth, differenceInMinutes, differenceInHours, minutesToHours, setMonth, getDate} from 'date-fns'
 import './App.css'
 
 export default function App() {
@@ -11,47 +10,74 @@ export default function App() {
   let [age, setAge] = useState(0);
   let [untilEvent, setUntilEvent] = useState([]);
   let [beforeAfter, setBeforeAfter] = useState(true)
+  let [eventName, setEventName] = useState('')
   const { register, formState: { errors }, handleSubmit, reset } = useForm({ criteriaMode: "all" });
-
   const diffDates = (d1, d2) => Math.floor((d1 - d2) / (365.25 * 24 * 60 * 60 * 1000));
 
   const timeCounter = (date1, date2) => {
     let firstDate = new Date(date1)
     let secondDate = new Date(date2)
-    
     let diffInMinutes = differenceInMinutes(secondDate, firstDate)
     let diffInHours = differenceInHours(secondDate, firstDate)
-    let minutes = diffInMinutes - diffInHours*60
+    let minutes = diffInMinutes - diffInHours * 60
     let hours = minutesToHours(diffInMinutes)
     let days = 0
     let months = 0
     let years = 0
 
-    while( hours>23) {
-      days++
-      hours -=24
-    }
-    while(days >= getDaysInMonth(new Date(getYear(secondDate) ,getMonth(secondDate) - 1))){ 
-      secondDate = setMonth(secondDate, getMonth(secondDate)-1)
-      months++
-      days -= getDaysInMonth(new Date(secondDate))
-    }
-    while(months >=12) {
-      years++
-      months -=12
+    if (isAfter(firstDate, secondDate)) {
+      let temp
+      temp = firstDate
+      firstDate = secondDate
+      secondDate = temp
+      diffInMinutes = differenceInMinutes(secondDate, firstDate)
+      diffInHours = differenceInHours(secondDate, firstDate)
+      minutes = diffInMinutes - diffInHours * 60
+      hours = minutesToHours(diffInMinutes)
+      while (hours > 23) {
+        days++
+        hours -= 24
+      }
+      while (days >= getDaysInMonth(new Date(getYear(firstDate), getMonth(firstDate) + 1))) {
+        firstDate = setMonth(firstDate, getMonth(firstDate) + 1)
+        months++
+        days -= getDaysInMonth(new Date(firstDate))
+      }
+      while (months >= 12) {
+        years++
+        months -= 12
+      }
+      setBeforeAfter(false)
+    } else {
+      while (hours > 23) {
+        days++
+        hours -= 24
+      }
+      while (days >= getDaysInMonth(new Date(getYear(secondDate), getMonth(secondDate) - 1))) {
+        secondDate = setMonth(secondDate, getMonth(secondDate) - 1)
+        months++
+        days -= getDaysInMonth(new Date(secondDate))
+      }
+      while (months >= 12) {
+        years++
+        months -= 12
+      }
+      setBeforeAfter(true)
     }
     setUntilEvent([years, months, days, hours, minutes])
   }
 
   const onSubmit = (data) => {
     let userAge = diffDates(new Date(data.currentDate), new Date(data.birthDate))
-    let currentDate = set(new Date(data.currentDate), {hours: getHours(new Date()), minutes:  getMinutes(new Date())})
-    let valuableEvent = new Date(data.valuableEvent)
+    let currentDate = set(new Date(data.currentDate), { hours: getHours(new Date()), minutes: getMinutes(new Date()) })
+    let valuableEvent = set(new Date(), {year: getYear(new Date(data.valuableEventDate)), month: getMonth(new Date(data.valuableEventDate)), date: getDate(new Date(data.valuableEventDate)),
+                                         hours: data.valuableEventTime.split(":")[0], minutes: data.valuableEventTime.split(":")[1]})
     timeCounter(currentDate, valuableEvent)
     if (userAge > 0 && userAge < 130) {
       setAge(userAge)
       setIsSubmited(true)
-    } 
+    }
+    setEventName(data.valuableEventName)
   }
   return (
     <>
@@ -95,39 +121,34 @@ export default function App() {
           }}
         />
         <label htmlFor='valuableEvent'>Event</label>
-        <input type='datetime-local' {...register("valuableEvent", {
-          maxLength: {
-            value: 16,
-            message: 'Max year is 9999'
-          },
-          required: "This input is required."
-        })} />
-        <ErrorMessage
-          errors={errors}
-          name="valuableEvent"
-          render={({ messages }) => {
-            return messages
-              ? Object.entries(messages).map(([type, message]) => (
-                <p key={type}>{message}</p>
-              ))
-              : null;
-          }}
-        />
+        <div className='valuableEvent'>
+          <span>Название</span>
+          <span>Дата</span>
+          <span>Время</span>
+        </div>
+        <div className='valuableEvent'>
+          <input {...register("valuableEventName", 
+                {required: "This input is required."})} type='text'/>
+          <input {...register("valuableEventDate", 
+                {required: "This input is required."})} type='date'/>
+          <input {...register("valuableEventTime", 
+                {required: "This input is required."})} type='time'/>
+        </div>
+        
         <input type="submit" />
         <input
-        type="button"
-        onClick={() =>
-          reset({
-            birthDate: "",
-            currentDate: "",
-            valuableEvent: "",
-          })
-        }
-        value="Reset inputs"
-      />
-      
+          type="button"
+          onClick={() =>
+            reset({
+              birthDate: "",
+              currentDate: "",
+              valuableEvent: "",
+            })
+          }
+          value="Reset inputs"
+        />
       </form>
-      {isSubmited && <UserInfo age={age} untilEvent={untilEvent} beforeAfter={beforeAfter} />}
+      {isSubmited && <UserInfo age={age} untilEvent={untilEvent} beforeAfter={beforeAfter} eventName={eventName}/>}
     </>
   );
 }
--- src/components/UserInfo.jsx
+++ src/components/UserInfo.jsx
@@ -6,8 +6,8 @@ let UserInfo = (props) => {
 
         <h1>Возраст пользователя: {props.age} years</h1>
         <h1>
-            {props.beforeAfter && `До события ${props.untilEvent[0]} years, ${props.untilEvent[1]} months, ${props.untilEvent[2]} days, ${props.untilEvent[3]} hours: ${props.untilEvent[4]} minutes`} 
-            {!props.beforeAfter && `После события прошло ${-props.untilEvent[0]} years, ${-props.untilEvent[1]} months, ${-props.untilEvent[2]} days, ${-props.untilEvent[3]} hours: ${-props.untilEvent[4]} minutes`} 
+            {props.beforeAfter && `До события "${props.eventName}" осталось: ${props.untilEvent[0]} years, ${props.untilEvent[1]} months, ${props.untilEvent[2]} days, ${props.untilEvent[3]} hours: ${props.untilEvent[4]} minutes`} 
+            {!props.beforeAfter && `После события "${props.eventName}" прошло: ${props.untilEvent[0]} years, ${props.untilEvent[1]} months, ${props.untilEvent[2]} days, ${props.untilEvent[3]} hours: ${props.untilEvent[4]} minutes`} 
         </h1>
     </>
     )

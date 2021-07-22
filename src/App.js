c6c620ac6beb00b2332861ba111617756ec9686d
Artem Zinovev <artemzinovev688@gmail.com>
2021-07-22 08:33:31 +0300

    подсчет от минут

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { useForm } from "react-hook-form";
 import UserInfo from "./components/UserInfo";
 import { ErrorMessage } from "@hookform/error-message";
-import { getMinutes, getHours, getDate, getMonth, getYear, yearsToMonths, isAfter, isEqual, set, differenceInMonths, differenceInDays, differenceInYears, getDaysInYear, getDaysInMonth, monthsToYears, addYears, differenceInMinutes, } from 'date-fns'
+import { getMinutes, getHours, getDate, getMonth, getYear, yearsToMonths, isAfter, isEqual, set, differenceInMonths, differenceInDays, differenceInYears, getDaysInYear, getDaysInMonth, monthsToYears, addYears, differenceInMinutes, differenceInHours, minutesToHours, } from 'date-fns'
 import ru from "date-fns/locale/ru"
 import './App.css'
 
@@ -17,98 +17,37 @@ export default function App() {
   const countYears = (d1, d2) => (yearsToMonths(getYear(d1)) + (getMonth(d1) + 1) - (yearsToMonths(getYear(d2)) + (getMonth(d2) + 1))) / 12
 
   const timeCounter = (date1, date2) => {
-    let testDate1 = new Date(date1)
-    let testDate2 = new Date(date2)
+    let firstDate = new Date(date1)
+    let secondDate = new Date(date2)
 
-    let diffMinutes = getMinutes(testDate2) - getMinutes(testDate1)
-    let diffHours = getHours(testDate2) - getHours(testDate1)
-    let diffDays = differenceInMinutes(testDate2, testDate1)/60/24 
-    // console.log(diffDays);
-    let diffYears = differenceInYears(testDate2, testDate1)                       
-    let diffMonths = differenceInMonths(testDate2, testDate1) - diffYears*12      
+    let diffInMinutes = differenceInMinutes(secondDate, firstDate)
+    let diffInHours = differenceInHours(secondDate, firstDate)
+    let diffInDays = differenceInDays(secondDate, firstDate)
+    let diffInMonths = differenceInMonths(secondDate, firstDate)
+    let diffInYears = differenceInYears(secondDate, firstDate)
 
-    
-    const daysInYearsCounter = (date1, date2) => {
-      let date1Year = getYear(date1)
-      // let date2Year = getYear(date2) 
-      let yearsTotal = differenceInDays(date2, date1)
-      // console.log(yearsTotal);
-      let fullYearsList = 0
+    let minutes = diffInMinutes - diffInHours*60
+    let hours = minutesToHours(diffInMinutes)
+    let days = 0
+    let months = 0
+    let years = 0
 
-      for(let i = date1Year; yearsTotal >= getDaysInYear(new Date(date2)); i++) {
-        yearsTotal -= getDaysInYear(new Date(i, 0, 1))
-        fullYearsList += getDaysInYear(new Date(i, 0, 1))
-      }
-      // console.log(fullYearsList);
-      return fullYearsList
+    while( hours>23) {
+      days++
+      hours -=24
     }
+    console.log(getDaysInMonth(new Date(getYear(secondDate) ,getMonth(secondDate))));
+    // while(days > getDaysInMonth(new Date(getYear(secondDate) ,getMonth(secondDate)))){ 
+      
+    //   months++
+    //   days -= getDaysInMonth(new Date(secondDate))
+    // }
 
-    const daysInMonthCounter = (date1, date2) => {
-      let date1Month = getMonth(date1)
-      let date2Month = getMonth(date2)
-      let date1Year = getYear(date1)
-      let date2Year = getYear(date2)
-      let fullMonthsList = 0;
-      let incompleteYear = 0
-
-      for( let i = date1Year; i <= date2Year; i++) if(getDaysInYear(i) > differenceInDays(date2, new Date(i, 0, 1))) incompleteYear = i
-    
-      if(getMonth(date1) > getMonth(date2)) {
-        // console.log(incompleteYear);
-        console.log(1)
-        for(let i = getMonth(date1) +1; i<=11; i++) {
-          fullMonthsList += getDaysInMonth(new Date(incompleteYear-1, i, 1))
-        }
-
-        for(let i = 0; i<=getMonth(date2); i++) {
-          fullMonthsList += getDaysInMonth(new Date(incompleteYear, i, 1))
-        }
-      }
-
-      if(getMonth(date1) <= getMonth(date2)) {
-        console.log(incompleteYear);
-
-        for(let i = getMonth(date1); i<getMonth(date2); i++ ) {
-          fullMonthsList += getDaysInMonth(new Date(incompleteYear, i, 1))
-        }
-        for(let i = 0; i<getMonth(date2); i++ ) {
-          fullMonthsList += getDaysInMonth(new Date(getYear(date2), i, 1))
-        }
-      }
-      console.log(fullMonthsList);
-      return fullMonthsList
-    }
-
-    daysInMonthCounter(testDate1, testDate2)
-
-    diffDays -= daysInYearsCounter(testDate1, testDate2)
-    diffDays -= daysInMonthCounter(testDate1, testDate2)
 
 
 
     
-
-    if(diffMinutes < 0) {
-      diffHours--
-      diffMinutes += 60
-    }
-
-    if(diffHours < 0) {
-      diffDays--
-      diffHours +=24
-    }
-
-    if(diffDays<0) {
-      diffMonths--
-      diffDays = getDaysInMonth(new Date(getYear(date2), getMonth(date2))) + diffDays
-    }
-
-    if(diffMonths<0) {
-      diffYears--
-      diffMonths = 12 + diffMonths
-    }
-    
-    let countedDiff = [diffYears, diffMonths, Math.floor(diffDays), diffHours, diffMinutes]
+    let countedDiff = [years, months, days, hours, minutes]
     console.log(countedDiff)
     setUntilEvent(countedDiff)
   }

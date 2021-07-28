a643acde99c06b2c74b076fd90ff657d82be9cb7
Amazingwhite <artemzinovev688@gmail.com>
2021-07-28 16:41:41 +0300

    калькулятор с баговаными стилями

---
+++ src/components/Calculator.jsx
@@ -0,0 +1,258 @@
+import { Button, makeStyles } from '@material-ui/core';
+import Slider from '@material-ui/core/Slider';
+import { useState } from 'react';
+import '../styles/Calculator.css';
+
+const useStyles = makeStyles({
+    sliderButton: {
+        color: 'rgba(0, 0, 0, 0.87)',
+        padding: 0,
+        minWidth: 0,
+        boxSizing: 'border-box',
+        margin: 4,
+        fontSize: 11,
+        backgroundColor: 'rgba(21, 34, 66, 0.06)',
+        borderRadius: 16
+    },
+    applyButton: {
+        backgroundColor: '#0468FF',
+        color: '#fff',
+        height: 48,
+        fontSize: 14,
+        font: 'Lato, Roboto, Arial, sans-serif',
+        fontWeight: 'bold',
+        '&:hover': {
+            backgroundColor: '#0248b2'
+        }
+    },
+    switchCalculatorButton: {
+        padding: '6px 8 px',
+        fontWeight: 400,
+        color: '#0468FF',
+        flexGrow: 1,
+        fontSize: 14,
+        lineHeight: '22px'
+    }
+});
+
+export default function Calculator() {
+
+    const styles = useStyles();
+
+    const [estateValue, setEstateValue] = useState(500000);
+    const [firstDepositValue, setFirstDepositValue] = useState(0);
+    const [loanPeriorValue, setLoanPeriorValue] = useState(1);
+    const [interestRateValue, setInterestRateValue] = useState(1);
+    
+    let monthlyRate = interestRateValue/12/100;
+    let loanPeriodMonths = (loanPeriorValue*12);
+    let totalBid = ((1 + monthlyRate)**loanPeriodMonths);
+    let monthlyPayment = ((estateValue-firstDepositValue)*monthlyRate*(totalBid/(totalBid - 1)));
+    let percents = Math.round((monthlyPayment*loanPeriodMonths - (estateValue-firstDepositValue)));
+    let minIncome = Math.round(((estateValue - firstDepositValue + percents)/12)*1.667);
+  
+    if (firstDepositValue < 0) setFirstDepositValue(0);
+    if (loanPeriorValue < 1) setLoanPeriorValue(1);
+    if (interestRateValue < 1) setInterestRateValue(1);
+
+    const numberWithSpaces = (x) => {
+        return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ");
+    };
+
+    const isStartsWithZero = (event) => {
+        event.target.value = Number.parseInt(event.target.value.toString());
+    };
+
+    const isValidEstateValue = (event) => {
+        if (estateValue < 500000) setEstateValue(500000);
+        isStartsWithZero(event);
+    };
+
+    const handleEstateSliderChange = (event, newEstateValue) => {
+        firstDepositValue >= newEstateValue - 500000 ? setFirstDepositValue(newEstateValue - 500000) : setFirstDepositValue(firstDepositValue);
+        setEstateValue(newEstateValue);
+    };
+
+    const handleEstateInputChange = (event) => {
+        firstDepositValue >= +event.target.value - 500000 ? setFirstDepositValue(+event.target.value - 500000) : setFirstDepositValue(firstDepositValue);
+        +event.target.value >= 99999999 ? setEstateValue(99999999) : setEstateValue(+event.target.value);
+    };
+
+    const handleFirstDepositSliderChange = (event, newFirstDepositValue) => {
+        setFirstDepositValue(newFirstDepositValue);
+    };
+
+    const handleFirstDepositInputChange = (event) => { 
+        +event.target.value > estateValue - 500000 ? setFirstDepositValue(estateValue - 500000) : setFirstDepositValue(+event.target.value);
+    };
+
+    const firstDepositPercentCounter = (event) => {
+        setFirstDepositValue((estateValue - 500000) * event.currentTarget.value)
+    };
+
+    const handleLoanPeriorSliderChange = (event, newLoanPeriodValue) => {
+        setLoanPeriorValue(newLoanPeriodValue);
+    };
+
+    const handleLoanPeriodInputChange = (event) => {
+        +event.target.value > 30 ? setLoanPeriorValue(30) : setLoanPeriorValue(+event.target.value);
+    };
+
+    const changeLoanPeriod = (event) => {
+        setLoanPeriorValue(+event.currentTarget.value);
+    };
+
+    const handleInterestRateSliderChange = (event, newInterestRateValue) => {
+        setInterestRateValue(newInterestRateValue);
+    };
+
+    const handleInterestRateInputChange = (event) => {
+        +event.target.value > 30 ? setInterestRateValue(30):setInterestRateValue(+event.target.value);
+    };
+
+    const changeInterestRate = (event) => {
+        setInterestRateValue(+event.currentTarget.value);
+    };
+
+    return (
+            <div className='container'>
+                <div className='leftPanel'>
+                    <div className='switchCalculator'>
+                        <Button className={styles.switchCalculatorButton}>Недвижимость</Button>
+                        <Button className={styles.switchCalculatorButton}>Кредит</Button>
+                        <Button className={styles.switchCalculatorButton}>Платеж</Button>
+                    </div>
+
+                    <div className='estateCost'>
+                        <p>Стоимость недвижимости</p>
+                        <input className='estateInput' 
+                               type="number" 
+                               inputMode='numeric' 
+                               value={estateValue} 
+                               onChange={handleEstateInputChange} 
+                               onBlur={isValidEstateValue}/>
+                        <Slider
+                            min={500000}
+                            max={99999999}
+                            step={280000}
+                            defaultValue={500000}
+                            onChange={handleEstateSliderChange}
+                            value={estateValue} />
+                    </div>
+
+                    <div className='firstDeposit'>
+                        <p>Первоначальный взнос</p>
+                        <input
+                            className='firstDepositInput'
+                            type="number"
+                            inputMode='numeric'
+                            value={firstDepositValue}
+                            onChange={handleFirstDepositInputChange} 
+                            onBlur={isStartsWithZero}/>
+                        <Slider
+                            min={0}
+                            max={estateValue - 500000}
+                            step={10000}
+                            defaultValue={0}
+                            onChange={handleFirstDepositSliderChange}
+                            value={firstDepositValue <= estateValue - 500000 ? firstDepositValue : estateValue - 500000} />
+
+                        <div className='firstDepositButtons'>
+                            <Button className={styles.sliderButton} onClick={firstDepositPercentCounter} value={0}>0%</Button>
+                            <Button className={styles.sliderButton} onClick={firstDepositPercentCounter} value={0.1}>10%</Button>
+                            <Button className={styles.sliderButton} onClick={firstDepositPercentCounter} value={0.15}>15%</Button>
+                            <Button className={styles.sliderButton} onClick={firstDepositPercentCounter} value={0.2}>20%</Button>
+                            <Button className={styles.sliderButton} onClick={firstDepositPercentCounter} value={0.25}>25%</Button>
+                            <Button className={styles.sliderButton} onClick={firstDepositPercentCounter} value={0.3}>30%</Button>
+                        </div>
+                    </div>
+
+                    <div className='loanPeriod'>
+                        <p>Срок кредита</p>
+                        <input className='loanPeriodInput'
+                               type="number" 
+                               inputMode='numeric' 
+                               onChange={handleLoanPeriodInputChange} 
+                               value={loanPeriorValue} 
+                               onBlur={isStartsWithZero}/>
+                        <Slider
+                            min={1}
+                            max={30}
+                            step={1}
+                            defaultValue={1}
+                            onChange={handleLoanPeriorSliderChange}
+                            value={loanPeriorValue} />
+
+                        <div className='loanPeriodButtons'>
+                            <Button className={styles.sliderButton} onClick={changeLoanPeriod} value={5}>5 лет</Button>
+                            <Button className={styles.sliderButton} onClick={changeLoanPeriod} value={10}>10 лет</Button>
+                            <Button className={styles.sliderButton} onClick={changeLoanPeriod} value={15}>15 лет</Button>
+                            <Button className={styles.sliderButton} onClick={changeLoanPeriod} value={20}>20 лет</Button>
+                        </div>
+                    </div>
+
+                    <div className='interestRate'>
+                        <p>Процентная ставка</p>
+                        <input className='interestRateInput'
+                               type="number" 
+                               inputMode='numeric' 
+                               value={interestRateValue} 
+                               onChange={handleInterestRateInputChange} 
+                               onBlur={isStartsWithZero}/>
+                        <Slider
+                            min={1}
+                            max={30}
+                            step={0.1}
+                            defaultValue={1}
+                            onChange={handleInterestRateSliderChange}
+                            value={interestRateValue} />
+
+                        <div className='loanPeriodButtons'>
+                            <Button className={styles.sliderButton} onClick={changeInterestRate} value={4.5}>4,5%</Button>
+                            <Button className={styles.sliderButton} onClick={changeInterestRate} value={6}>6%</Button>
+                            <Button className={styles.sliderButton} onClick={changeInterestRate} value={7.5}>7,5%</Button>
+                            <Button className={styles.sliderButton} onClick={changeInterestRate} value={9.1}>9,1%</Button>
+                            <Button className={styles.sliderButton} onClick={changeInterestRate} value={10}>10%</Button>
+                        </div>
+                    </div>
+                </div>
+                <div className='rightPanel'>
+                    <p>Ваш ежемесячный платеж</p>
+                    <div className='monthlyPaymentCounter'>
+                        <h1>{numberWithSpaces(monthlyPayment.toFixed())} ₽</h1>
+                    </div>
+                    <div className='rightPanelElement'>
+                        <p>Кредит</p>
+                        <div >
+                            <p>{numberWithSpaces(estateValue - firstDepositValue)} ₽</p>
+                        </div>
+                    </div>
+
+                    <div className='rightPanelElement'>
+                        <p>Проценты</p>
+                        <div >
+                            <p>{numberWithSpaces(percents)} ₽</p>
+                        </div>
+                    </div>
+
+                    <div className='rightPanelElement'>
+                        <p>Проценты+кредит</p>
+                        <div >
+                            <p>{numberWithSpaces(estateValue - firstDepositValue + percents)} ₽</p>
+                        </div>
+                    </div>
+
+                    <div className='rightPanelElement'>
+                        <p>Необходимый доход</p>
+                        <div >
+                            <p>{numberWithSpaces(minIncome)}</p>
+                        </div>
+                    </div>
+                    
+                    <Button href='http://www.yandex.ru' variant='contained' className={styles.applyButton}>
+                        Подать заявку онлайн
+                    </Button>
+                </div>
+            </div>
+    );
+}
---
+++ src/components/Form.jsx
@@ -0,0 +1,134 @@
+import React, { useState } from "react";
+import { useForm, useFieldArray } from "react-hook-form";
+import { UserInfo } from "./UserInfo";
+import { ErrorMessage } from "@hookform/error-message";
+import moment from 'moment';
+import { Link } from "react-router-dom";
+import '../styles/FormStyle.css'
+
+export const Form = (props) => {
+    let allDiffs = []
+    let [isSubmited, setIsSubmited] = useState(false);
+    let [age, setAge] = useState(0);
+    let [untilEvent, setUntilEvent] = useState([]);
+    const { register, control, formState: { errors }, handleSubmit, reset } = useForm({
+      criteriaMode: "all",
+      defaultValues: {
+        events: [{ eventName: "some name", eventDate: "", eventTime: "" }]
+      }
+    });
+    const { fields, append } = useFieldArray({control, name: "events"});
+    const timeCounter = (date1, date2) => {
+      if (moment(date1) > moment(date2)) {
+        const diffDate = moment.duration(moment(date1).diff(moment(date2))).add(1, 'minutes')
+        allDiffs.push({ beforeAfter: false, years: diffDate.years(), months: diffDate.months(), days: diffDate.days(), hours: diffDate.hours(), minutes: diffDate.minutes() })
+      } else {
+        const diffDate = moment.duration(moment(date2).diff(moment(date1)))
+        allDiffs.push({ beforeAfter: true, years: diffDate.years(), months: diffDate.months(), days: diffDate.days(), hours: diffDate.hours(), minutes: diffDate.minutes() })
+      }
+    }
+    const onSubmit = (data) => {
+      let currentDate = moment(data.currentDate).set({ hour: moment().get('hour'), minute: moment().get('minute') })
+      data.events.forEach((i) => {
+        let valuableEvent = moment().set({
+          year: moment(i.eventDate).get('year'),
+          month: moment(i.eventDate).get('month'),
+          date: moment(i.eventDate).get('date'),
+          hour: i.eventTime.split(":")[0],
+          minute: i.eventTime.split(":")[1]
+        })
+        timeCounter(currentDate, valuableEvent)
+        allDiffs[allDiffs.length-1].eventName = i.eventName;
+      })
+      setAge(moment(data.currentDate).diff(data.birthDate, 'year')) 
+      setUntilEvent(allDiffs)
+      setIsSubmited(true)
+    }
+    return (
+      <>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <label htmlFor='birthDate'>Birth date</label>
+          <input type='date' {...register("birthDate", {
+            maxLength: {
+              value: 10,
+              message: 'Max year is 9999'
+            },
+            required: "This input is required."
+          })} />
+          <ErrorMessage
+            errors={errors}
+            name="birthDate"
+            render={({ messages }) => {
+              return messages
+                ? Object.entries(messages).map(([type, message]) => (
+                  <p key={type}>{message}</p>
+                )) : null;
+            }}/>
+          <label htmlFor='currentDate'>Current date</label>
+          <input type='date' {...register("currentDate", {
+            maxLength: {
+              value: 10,
+              message: 'Max year is 9999'
+            },
+            required: "This input is required."
+          })} />
+          <ErrorMessage
+            errors={errors}
+            name="currentDate"
+            render={({ messages }) => {
+              return messages
+                ? Object.entries(messages).map(([type, message]) => (
+                  <p key={type}>{message}</p>
+                )) : null;
+            }}/>
+          <label htmlFor='valuableEvent'>Events</label>
+          {fields.map((item, index) => {
+            return (
+              <li className='valuableEvent' key={item.id}>
+                <input
+                  {...register(`events.${index}.eventName`)}
+                  type='text'
+                  name={`events[${index}].eventName`}
+                  defaultValue={`${item.eventName}`}/>
+                <input
+                  {...register(`events.${index}.eventDate`)}
+                  type='date'
+                  name={`events[${index}].eventDate`}
+                  defaultValue={`${item.eventDate}`}/>
+                <input
+                  {...register(`events.${index}.eventTime`)}
+                  type='time'
+                  name={`events[${index}].eventTime`}
+                  defaultValue={`${item.eventTime}`}/>
+              </li>
+            );
+          })}
+          <button
+            type="button"
+            onClick={() => {
+              append({ eventName: "appendEventName", eventDate: "", eventTime: "" });
+            }}>
+            Add Event
+          </button>
+          <input type="submit" />
+          <input
+            type="button"
+            onClick={() =>
+              reset({
+                birthDate: "",
+                currentDate: "",
+                valuableEvent: "",
+                events: [{eventName: "some name", eventDate: "", eventTime: ""}]
+              })}
+            value="Reset inputs"/>
+            <Link to="/calculator">
+            <input 
+              type="button"
+              value="Залезть в долги" />
+            </Link>
+        </form>
+        
+        {isSubmited && <UserInfo age={age} untilEvent={untilEvent} />}
+      </>
+    );
+}

9e6a47af210b81f27b440351429ac0c67d6f2632
Amazingwhite <artemzinovev688@gmail.com>
2021-07-29 17:57:22 +0300

    calc

--- src/App.js
+++ src/App.js
@@ -3,13 +3,16 @@ import { Form } from "./components/Form";
 import { Route } from "react-router";
 import {Calculator}  from "./components/Calculator";
 import { Redirect } from "react-router-dom";
+import { BrowserRouter } from 'react-router-dom';
 
 export default function App() {
   return(
     <>
-      <Route path='/date' component={Form} />
-      <Route path='/calculator' component={Calculator} />
-      <Redirect from='/' to='/date' />
+      <BrowserRouter>
+        <Route path='/date' component={Form} />
+        <Route path='/calculator' component={Calculator} />
+        <Redirect from='/' to='/date' />
+      </BrowserRouter>
     </>
   )
   
